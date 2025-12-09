57ba7e6ed38302b498aa248940a6774869fcd5be
MS-kise <cist.b220.m.sudou@gmail.com>
2025-12-09 21:13:21 +0900

    変数の命名を揃えた

--- src/main/java/org/example/ticket/domain/小計計算式.java
+++ src/main/java/org/example/ticket/domain/小計計算式.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class 小計計算式 {
     public 小計 計算する(食券明細 食券明細, 取扱食券の一覧 取扱食券の一覧) {
-        int 小計 = 0;
+        int 小計の値 = 0;
         for (食券明細項目 食券明細項目 : 食券明細.get食券明細項目の一覧()){
             取扱食券 対応する取扱食券 = null;
             for (取扱食券 取扱食券 : 取扱食券の一覧.get食券の一覧()) {
@@ -18,8 +18,8 @@ public class 小計計算式 {
             int 単価 = 対応する取扱食券.get単価();
             int 枚数 = 食券明細項目.get枚数();
 
-            小計 += 単価 * 枚数;
+            小計の値 += 単価 * 枚数;
         }
-        return new 小計(小計);
+        return new 小計(小計の値);
     }
 }
--- src/main/java/org/example/ticket/domain/獲得ポイント計算式.java
+++ src/main/java/org/example/ticket/domain/獲得ポイント計算式.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class 獲得ポイント計算式 {
     public 獲得ポイント数 計算する(まとめ買い割引後の金額 まとめ買い割引後の金額) {
-        int 獲得ポイント数 = 50 * (int) Math.floor(まとめ買い割引後の金額.get金額() / 500);
-        return new 獲得ポイント数(獲得ポイント数);
+        int 獲得ポイント数の値 = 50 * (int) Math.floor(まとめ買い割引後の金額.get金額() / 500);
+        return new 獲得ポイント数(獲得ポイント数の値);
     }
 }
